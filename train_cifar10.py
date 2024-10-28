096ff6565f88ecf40e2a16e85e2579cc471386cc
Ahmed-Essam <ahmed.elhlwgy@gmail.com>
2024-10-28 18:03:47 +0300

    Use Pytorch Lightning

--- train_cifar10.py
+++ train_cifar10.py
@@ -2,7 +2,7 @@ import torch
 
 from torchvision.transforms import Resize, CenterCrop, ToTensor, Compose, Lambda, ToPILImage
 from torch.utils.data import DataLoader
-from torchvision.utils import save_image
+from torchvision.utils import make_grid
 from tqdm import tqdm
 from torch.optim import Adam
 from torch.nn import MSELoss
@@ -11,8 +11,11 @@ from pathlib import Path
 from dataset import CIFAR10Dataset
 from diffusion_utils import DiffusionUtils
 from model import UNet
+from lightning.pytorch.callbacks import Callback
+from lightning.pytorch.loggers import TensorBoardLogger
 
 import numpy as np
+import lightning as L
 
 
 IMAGE_SIZE = 128
@@ -26,7 +29,7 @@ FIRST_LAYER_CHANNELS = 128
 CHANNELS_MULTIPLIER = [1, 2, 2, 2]
 NUM_RES_BLOCKS = 2
 ATTN_RESOLUTIONS = [16]
-LOG_INTERVAL = 100
+LOG_INTERVAL = 10
 RESULTS_FOLDER = Path("./results")
 RESULTS_FOLDER.mkdir(exist_ok=True)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -51,44 +54,53 @@ def main():
 
     dataset = CIFAR10Dataset(timesteps=TIMESTEPS, transform=transform, train=True)
     dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
-    model = UNet(IMAGE_SIZE, FIRST_LAYER_CHANNELS, CHANNELS_MULTIPLIER, NUM_RES_BLOCKS, ATTN_RESOLUTIONS)
-    model.to(DEVICE)
-    diffusion = DiffusionUtils(TIMESTEPS, DEVICE)
-    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
-    loss_fn = MSELoss()
-
-    for epoch in range(EPOCHS):
-        for step, batch in tqdm(enumerate(dataloader), total=len(dataset) // BATCH_SIZE):
-            optimizer.zero_grad()
-
-            x_0, t = batch
-            x_0 = x_0.to(DEVICE)
-            t = t.to(DEVICE)
-            t = t.squeeze(1)
-            
-            noise = torch.randn_like(x_0).to(DEVICE)
-            x_t = diffusion.q_sample(x_0, t, noise)
-            
-            pred = model(x_t, t)
-            loss = loss_fn(pred, noise)
-            loss.backward()
-            optimizer.step()
-
-            if step % LOG_INTERVAL == 0:
-                print(f"\nEpoch {epoch} Step {step} Loss: {loss.item()}")
-
-            # save generated images
-            if step != 0 and step % LOG_INTERVAL == 0:
-                model.eval()
-                milestone = step // LOG_INTERVAL
-                all_images = diffusion.p_sample_loop(model, (VIEW_SAMPLE_SIZE, 3, IMAGE_SIZE, IMAGE_SIZE))
-                print(all_images.shape)
-                all_images = (all_images + 1) * 0.5
-                save_image(all_images, str(RESULTS_FOLDER / f'sample-{milestone}.png'), nrow = 1)
-                model.train()
+    diffusion_utils = DiffusionUtils(TIMESTEPS, DEVICE)
+    model = PLModel(diffusion_utils)
+    logger = TensorBoardLogger(save_dir=str(RESULTS_FOLDER))
+    image_generation_callback = ImageGenerationCallback(VIEW_SAMPLE_SIZE, LOG_INTERVAL, diffusion_utils)
+    trainer = L.Trainer(max_epochs=EPOCHS, accelerator=DEVICE, default_root_dir=str(RESULTS_FOLDER), log_every_n_steps=LOG_INTERVAL, logger=logger, callbacks=[image_generation_callback])
+    trainer.fit(model, dataloader)
 
 
-        torch.save(model.state_dict(), str(RESULTS_FOLDER / f'model-{epoch}.pth'))
+class PLModel(L.LightningModule):
+    def __init__(self, diffusion_utils: DiffusionUtils):
+        super().__init__()
+        self.model = UNet(IMAGE_SIZE, FIRST_LAYER_CHANNELS, CHANNELS_MULTIPLIER, NUM_RES_BLOCKS, ATTN_RESOLUTIONS)
+        self.loss_fn = MSELoss()
+        self.diffusion_utils = diffusion_utils
+
+    def forward(self, x, t):
+        return self.model(x, t)
+
+    def training_step(self, batch, batch_idx):
+        x_0, t = batch
+        t = t.squeeze(1)
+        noise = torch.randn_like(x_0).to(DEVICE)
+        x_t = self.diffusion_utils.q_sample(x_0, t, noise)
+        pred = self(x_t, t)
+        loss = self.loss_fn(pred, noise)
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
+    
+    def configure_optimizers(self):
+        return Adam(self.model.parameters(), lr=LEARNING_RATE)
+    
+
+class ImageGenerationCallback(Callback):
+    def __init__(self, num_samples, every_n_steps, diffusion_utils: DiffusionUtils):
+        super().__init__()
+        self.num_samples = num_samples
+        self.every_n_steps = every_n_steps
+        self.diffusion_utils = diffusion_utils
+
+    def on_train_batch_end(self, trainer: L.Trainer, pl_module: PLModel, outputs, batch, batch_idx):
+        if (trainer.global_step + 1) % self.every_n_steps == 0:
+            milestone = trainer.global_step // LOG_INTERVAL
+            all_images = self.diffusion_utils.p_sample_loop(pl_module, (VIEW_SAMPLE_SIZE, 3, IMAGE_SIZE, IMAGE_SIZE))
+            all_images = (all_images + 1) * 0.5
+            img = make_grid(all_images, nrow = 1)
+            logger: TensorBoardLogger = trainer.logger
+            logger.experiment.add_image(f"generated_images_{trainer.global_step}", img, milestone)
 
 
 if __name__ == "__main__":
