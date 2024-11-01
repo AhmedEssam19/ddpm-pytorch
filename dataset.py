4685236c39748d365ec5be5e7c5e75cc240471f2
Ahmed-Essam <a.essam@businext.com>
2024-11-01 23:51:47 +0200

    Support CelebA dataset

--- dataset.py
+++ dataset.py
@@ -1,5 +1,7 @@
 import torch
+import kagglehub
 
+from PIL import Image
 from torch.utils.data import Dataset
 from torchvision.datasets import CIFAR10
 
@@ -16,4 +18,19 @@ class CIFAR10Dataset(Dataset):
     
     def __getitem__(self, idx):
         image, _ = self.dataset[idx]
+        return self.transform(image), torch.randint(0, self.timesteps, (1,))
+    
+
+class CelebADataset(Dataset):
+    def __init__(self, timesteps, transform):
+        super().__init__()
+        self.timesteps = timesteps
+        self.transform = transform
+        self.path = kagglehub.dataset_download("badasstechie/celebahq-resized-256x256")
+
+    def __len__(self):
+        return 30000
+    
+    def __getitem__(self, idx):
+        image = Image.open(f"{self.path}/celeba_hq_256/{idx:05d}.jpg")
         return self.transform(image), torch.randint(0, self.timesteps, (1,))
---
+++ train_celeba.py
@@ -0,0 +1,54 @@
+import typer
+
+from torchvision.transforms import ToTensor, Compose, Lambda, ToPILImage, Resize, RandomHorizontalFlip
+from torch.utils.data import DataLoader
+from pathlib import Path
+from dataset import CelebADataset
+from diffusion_utils import DiffusionUtils
+
+from pl_utils import PLModel, ImageGenerationCallback
+from lightning.pytorch.loggers import TensorBoardLogger
+
+import numpy as np
+import lightning as L
+
+
+def main(
+    image_size: int = 256,
+    timesteps: int = 1000,
+    batch_size: int = 16,
+    view_sample_size: int = 10,
+    num_workers: int = 8,
+    max_steps: int = 500000,
+    learning_rate: float = 2e-5,
+    dropout: float = 0,
+    first_layer_channels: int = 128,
+    channels_multiplier: list[int] = [1, 1, 2, 2, 4, 4],
+    num_res_blocks: int = 2,
+    attn_resolutions: list[int] = [16],
+    log_interval: int = 200,
+    results_folder: str = "./results",
+    accelerator: str = "gpu",
+    num_gpus: int = 1
+):
+    results_folder = Path(results_folder)
+    results_folder.mkdir(exist_ok=True)
+    
+    transform = Compose([
+        RandomHorizontalFlip(),
+        ToTensor(),
+        Lambda(lambda x: (x * 2) - 1)
+    ])
+
+    dataset = CelebADataset(timesteps=timesteps, transform=transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    diffusion_utils = DiffusionUtils(timesteps)
+    model = PLModel(image_size, first_layer_channels, channels_multiplier, num_res_blocks, attn_resolutions, dropout, learning_rate, diffusion_utils)
+    logger = TensorBoardLogger(save_dir=str(results_folder))
+    image_generation_callback = ImageGenerationCallback(view_sample_size, image_size, log_interval, diffusion_utils)
+    trainer = L.Trainer(max_steps=max_steps, accelerator=accelerator, devices=num_gpus, default_root_dir=str(results_folder), log_every_n_steps=log_interval, logger=logger, callbacks=[image_generation_callback])
+    trainer.fit(model, dataloader)
+
+
+if __name__ == "__main__":
+    typer.run(main)
